3819ef56558231292eda2c61ea01f99522c57ad4
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:28:56 +0800

    adding s01/appy_s01_00_08.py

---
+++ s01/appy_s01_00_08.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:11:07 (UT+8) daisuke>
+#
+
+# assignment of variables
+a = 1.2
+b = 3.4
+
+# calculation
+c = a * b
+
+# printing the value of a variable using print () function
+print ('%f * %f = %f' % (a, b, c) )
