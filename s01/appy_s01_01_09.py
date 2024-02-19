189aae792604ad771bc76286c6b7947c87f66f05
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:29:18 +0800

    adding s01/appy_s01_01_09.py

---
+++ s01/appy_s01_01_09.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:12:23 (UT+8) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = 1000000
+
+# calculation of natural logarithm
+b = math.log10 (a)
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = log10 (a) = log10 ({a}) = {b}')
