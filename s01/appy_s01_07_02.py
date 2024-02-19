3aef8255fea2873a4d40fadcc0db0dbe5988954b
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:30:00 +0800

    adding s01/appy_s01_07_02.py

---
+++ s01/appy_s01_07_02.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:15:20 (UT+8) daisuke>
+#
+
+# two numbers
+a = 3
+b = 'abc'
+
+# calculation
+c = a / b
+
+# printing result of calculation
+print (f'{a} / {b} = {c}')
