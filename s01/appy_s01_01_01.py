3684f56e4cafef14950670e4c3ae74e6c2dc5b82
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:29:05 +0800

    adding s01/appy_s01_01_01.py

---
+++ s01/appy_s01_01_01.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:11:42 (UT+8) daisuke>
+#
+
+# two numbers
+a = 7
+b = 11
+
+# calculation
+c = a - b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a - b = {a} - {b} = {c}')
