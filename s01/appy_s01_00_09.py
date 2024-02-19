ba430213dac88b335b715e646ef306d5aa422a79
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:28:58 +0800

    adding s01/appy_s01_00_09.py

---
+++ s01/appy_s01_00_09.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:11:12 (UT+8) daisuke>
+#
+
+# assignment of a variable
+pi = 3.141592653589793
+
+# fancy formatting using formatted string literals
+print (f'pi = {pi:8.6f}')
