37f277cdb7133de58509ec72d24278ddcdff7ba6
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:29:03 +0800

    adding s01/appy_s01_01_00.py

---
+++ s01/appy_s01_01_00.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:11:36 (UT+8) daisuke>
+#
+
+# two numbers
+a = 3
+b = 5
+
+# calculation
+c = a + b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a + b = {a} + {b} = {c}')
