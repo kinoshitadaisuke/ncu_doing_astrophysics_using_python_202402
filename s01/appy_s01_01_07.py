02a27716ab9d22505b67a21b734a56ebb5e7b7bd
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:29:14 +0800

    adding s01/appy_s01_01_07.py

---
+++ s01/appy_s01_01_07.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:12:13 (UT+8) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = -1.0
+
+# calculation of exponential function
+b = math.exp (a)
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = exp (a) = {math.e}^{a} = {b}')
