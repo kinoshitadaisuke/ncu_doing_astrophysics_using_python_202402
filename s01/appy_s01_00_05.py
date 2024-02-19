f86090c51f5e5278d50429e12099d1319d74bfe5
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:28:51 +0800

    adding s01/appy_s01_00_05.py

---
+++ s01/appy_s01_00_05.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:10:51 (UT+8) daisuke>
+#
+
+# assignment of a variable
+a = 1.23 * 10**12
+
+# printing the value of a variable using print () function
+print ('a = %g' % a)
