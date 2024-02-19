034b01100871bf9d289a65db66444efb5e6686fa
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:28:48 +0800

    adding s01/appy_s01_00_03.py

---
+++ s01/appy_s01_00_03.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:10:40 (UT+8) daisuke>
+#
+
+# assignment of a variable
+a = 123.456
+
+# printing the value of a variable using print () function
+print ('a =', a)
