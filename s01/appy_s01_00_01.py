66fe13ebe641e797c59c10363148e3dc5647ee97
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-19 17:28:44 +0800

    adding s01/appy_s01_00_01.py

---
+++ s01/appy_s01_00_01.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/19 17:10:28 (UT+8) daisuke>
+#
+
+# assignment of a variable
+sentence = 'I love astroinformatics!'
+
+# printing the value of a variable using print () function
+print (sentence)
