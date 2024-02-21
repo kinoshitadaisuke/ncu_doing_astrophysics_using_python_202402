073ee112595f2b681255799b0834f8065093372b
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-21 15:52:16 +0800

    adding s02/appy_s02_13_00.py

---
+++ s02/appy_s02_13_00.py
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/21 15:26:11 (UT+8) daisuke>
+#
+
+# two floating point numbers "a" and "b"
+a = 1.2
+b = 2.4
+
+# calculation of c = a + b
+c = a + b
+
+# printing result of calculation
+print (f'{a} + {b}             = {c}')
+
+# the other calculations
+d = 1.1
+e = 1.1
+f = 1.1
+g = d + e
+h = d + e + f
+i = d + e + f - 3.3
+print (f'{d} + {e}             = {g}')
+print (f'{d} + {e} + {f}       = {h}')
+print (f'{d} + {e} + {f} - 3.3 = {i}')
