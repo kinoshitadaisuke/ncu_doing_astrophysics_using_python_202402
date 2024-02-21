6d791b526dea60fc9a0e8b8790386905c0f65d85
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-21 15:51:48 +0800

    adding s02/appy_s02_08_03.py

---
+++ s02/appy_s02_08_03.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/21 15:24:01 (UT+8) daisuke>
+#
+
+# importing datetime module
+import datetime
+
+# current time in UTC
+time_now_utc = datetime.datetime.utcnow ()
+
+# printing result
+print (f'current time in UTC = {time_now_utc}')
