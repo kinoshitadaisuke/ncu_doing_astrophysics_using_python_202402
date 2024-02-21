893664fde999a409683ba18ed15957c03524797c
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-21 15:52:01 +0800

    adding s02/appy_s02_10_00.py

---
+++ s02/appy_s02_10_00.py
@@ -0,0 +1,36 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/21 15:24:58 (UT+8) daisuke>
+#
+
+# importing math module
+import math
+
+#
+# some constants
+#
+
+# pi
+pi = math.pi
+print (f'pi   = {pi}')
+
+# tau
+tau = math.tau
+print (f'tau  = 2.0 * pi\n     = {tau}')
+
+# e
+e = math.e
+print (f'e    = {e}')
+
+# positive infinity
+pinf = math.inf
+print (f'+inf = {pinf}')
+
+# negative infinity
+ninf = -math.inf
+print (f'-inf = {ninf}')
+
+# NaN (not a number)
+nan = math.nan
+print (f'NaN  = {nan}')
