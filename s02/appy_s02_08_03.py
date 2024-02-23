aac50b54429bbe952e3b0654d76e10d66cf501d9
kinoshitadaisuke <mhdf23888@gmail.com>
2024-02-23 13:48:04 +0800

    updating s02/appy_s02_08_03.py

--- s02/appy_s02_08_03.py
+++ s02/appy_s02_08_03.py
@@ -1,14 +1,17 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/02/21 15:24:01 (UT+8) daisuke>
+# Time-stamp: <2024/02/23 12:45:05 (UT+8) daisuke>
 #
 
 # importing datetime module
 import datetime
 
+# timezone information (UT+0)
+tzinfo = datetime.timezone (datetime.timedelta (0.0), name='UT+0')
+
 # current time in UTC
-time_now_utc = datetime.datetime.utcnow ()
+time_now_utc = datetime.datetime.now (tz=tzinfo)
 
 # printing result
 print (f'current time in UTC = {time_now_utc}')
