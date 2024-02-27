61c2b2622f06929fb6a27e9d26da2b53775d609c
Daisuke <mhdf23888@gmail.com>
2024-02-27 09:03:49 +0800

    updating s02/appy_s02_12_00.py

--- s02/appy_s02_12_00.py
+++ s02/appy_s02_12_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/02/21 15:25:42 (UT+8) daisuke>
+# Time-stamp: <2024/02/27 08:57:54 (UT+8) daisuke>
 #
 
 # importing statistics module
@@ -19,40 +19,40 @@ print (f'dataset1:\n{dataset1}')
 mean = statistics.fmean (dataset1)
 
 # printing calculated mean
-print (f'mean of dataset1   = {mean}')
+print (f'mean of dataset1                          = {mean:6.3f}')
 
 # calculation of median
 median = statistics.median (dataset1)
 
 # printing calculated median
-print (f'median of dataset1 = {median}')
+print (f'median of dataset1                        = {median:6.3f}')
 
 # calculation of mode
 mode = statistics.mode (dataset1)
 
 # printing calculated mode
-print (f'mode of dataset1   = {mode}')
+print (f'mode of dataset1                          = {mode:6.3f}')
 
 # calculation of sample variance
 var = statistics.variance (dataset1)
 
 # printing calculated sample variance
-print (f'sample variance of dataset1               = {var}')
+print (f'sample variance of dataset1               = {var:6.3f}')
 
 # calculation of population variance
 pvar = statistics.pvariance (dataset1)
 
 # printing calculated population variance
-print (f'population variance of dataset1           = {pvar}')
+print (f'population variance of dataset1           = {pvar:6.3f}')
 
 # calculation of sample standard deviation
 stddev = statistics.stdev (dataset1)
 
 # printing calculated sample variance
-print (f'sample standard deviation of dataset1     = {stddev}')
+print (f'sample standard deviation of dataset1     = {stddev:6.3f}')
 
 # calculation of population standard deviation
 pstddev = statistics.pstdev (dataset1)
 
 # printing calculated population standard deviation
-print (f'population standard deviation of dataset1 = {pstddev}')
+print (f'population standard deviation of dataset1 = {pstddev:6.3f}')
