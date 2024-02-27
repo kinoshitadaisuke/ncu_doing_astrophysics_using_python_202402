442fe20f8c78d7ae5ab69b4a60a64c15c6ed947b
Daisuke <mhdf23888@gmail.com>
2024-02-27 16:17:06 +0800

    adding s03/appy_s03_08_05.py

---
+++ s03/appy_s03_08_05.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/02/27 16:09:50 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making Numpy array
+a = numpy.array ([5.0, 3.0, 7.0, 4.0, 9.0, 8.0, 1.0, 6.0, 2.0, 0.0])
+
+# printing "a"
+print (f'a:\n{a}')
+
+# in-place sorting by timsort using ".sort ()" method
+a.sort (kind='stable')
+
+# printing "a"
+print (f'a:\n{a}')
