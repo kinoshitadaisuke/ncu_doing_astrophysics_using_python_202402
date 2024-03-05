977b0838f3342a3345cfcb196dfb06c08ee5b0ba
Daisuke <mhdf23888@gmail.com>
2024-03-05 12:43:07 +0800

    updating s03/appy_s03_11_02.py

--- s03/appy_s03_11_02.py
+++ s03/appy_s03_11_02.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/02/27 16:11:16 (UT+8) daisuke>
+# Time-stamp: <2024/03/05 12:35:20 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -36,5 +36,5 @@ mean_a = numpy.mean (a)
 mean_a_masked = numpy.ma.mean (a_masked)
 
 # printing mean values
-print (f'numpy.mean (a) = {mean_a}')
+print (f'numpy.mean (a)           = {mean_a}')
 print (f'numpy.ma.mean (a_masked) = {mean_a_masked}')
