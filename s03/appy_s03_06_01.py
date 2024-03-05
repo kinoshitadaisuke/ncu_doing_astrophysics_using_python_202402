8868be46a3da2c0297c0520d02d488fd56f5cae9
Daisuke <mhdf23888@gmail.com>
2024-03-05 12:43:02 +0800

    updating s03/appy_s03_06_01.py

--- s03/appy_s03_06_01.py
+++ s03/appy_s03_06_01.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/02/27 16:08:42 (UT+8) daisuke>
+# Time-stamp: <2024/03/05 11:41:52 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -16,7 +16,7 @@ print (f'a:\n{a}')
 # accessing to an element by indexing
 print (f'a[0][0] = {a[0][0]}')
 print (f'a[1][2] = {a[1][2]}')
-print (f'a[1,2] = {a[1,2]}')
+print (f'a[1,2]  = {a[1,2]}')
 print (f'a[0,-1] = {a[0,-1]}')
-print (f'a[0] = {a[0]}')
-print (f'a[-1] = {a[-1]}')
+print (f'a[0]    = {a[0]}')
+print (f'a[-1]   = {a[-1]}')
