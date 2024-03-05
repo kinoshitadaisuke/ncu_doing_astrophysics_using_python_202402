e00d01751273698790da636cd1b1e77d89003638
Daisuke <mhdf23888@gmail.com>
2024-03-05 12:40:25 +0800

    updating s03/appy_s03_05_09.py

--- s03/appy_s03_05_09.py
+++ s03/appy_s03_05_09.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/02/27 16:07:55 (UT+8) daisuke>
+# Time-stamp: <2024/03/05 11:35:28 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -12,14 +12,14 @@ a = numpy.array ([1.0, 2.0])
 b = numpy.array ([3.0, 4.0])
 
 # printing a and b
-print (f'a = {a}')
-print (f'b = {b}')
+print (f'a     = {a}')
+print (f'b     = {b}')
 
 # dot product of two vectors
 dot = numpy.dot (a, b)
 
 # printing dot product
-print (f'dot = {dot}')
+print (f'dot   = {dot}')
 
 # inner product of two vectors
 inner = numpy.inner (a, b)
