3a86d561c65b6f7bb38bb8d31351abb0afd644a7
Daisuke <mhdf23888@gmail.com>
2024-03-06 10:27:08 +0800

    updating s04/appy_s04_06_02.py

--- s04/appy_s04_06_02.py
+++ s04/appy_s04_06_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/09/30 23:11:20 (CST) daisuke>
+# Time-stamp: <2024/03/06 10:22:08 (UT+8) daisuke>
 #
 
 # importing argparse module
