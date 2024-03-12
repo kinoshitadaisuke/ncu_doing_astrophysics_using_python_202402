edae697ac401ea6e07eae9cfac3eaa57491ddbad
Daisuke <mhdf23888@gmail.com>
2024-03-12 08:39:17 +0800

    updating s04/appy_s04_11_05.py

---
+++ s04/appy_s04_11_05.py
@@ -0,0 +1,31 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2024/03/12 08:35:04 (UT+8) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# frame rate
+framerate = 30
+
+# number of threads
+n_thread = 1
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -i solsys_3d3/%06d.png -start_number 0' \
+    + f' -framerate {framerate} -an -vcodec libx264 -pix_fmt yuv420p' \
+    + f' -threads {n_thread}'
+
+# output file name
+file_output = 'solsys_3d3.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
