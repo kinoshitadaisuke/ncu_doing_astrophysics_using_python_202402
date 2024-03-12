613bddac82827581fb9dbe6e381c5de34cb5bd63
Daisuke <mhdf23888@gmail.com>
2024-03-12 08:38:47 +0800

    updating s04/appy_s04_07_03.py

--- s04/appy_s04_07_03.py
+++ s04/appy_s04_07_03.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/06 10:22:36 (UT+8) daisuke>
+# Time-stamp: <2024/03/11 23:37:49 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -21,7 +21,8 @@ import matplotlib.backends.backend_agg
 import matplotlib.figure
 
 # constructing a parser object
-parser = argparse.ArgumentParser (description='Making a histogram')
+descr  = 'making a histogram and generating a plot'
+parser = argparse.ArgumentParser (description=descr)
 
 # adding arguments
 parser.add_argument ('-o', '--output', default='output.png', \
