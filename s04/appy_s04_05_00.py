6b24807b0a6c3ee349c00f9982f1d13479714c27
Daisuke <mhdf23888@gmail.com>
2024-03-12 08:38:36 +0800

    updating s04/appy_s04_05_00.py

--- s04/appy_s04_05_00.py
+++ s04/appy_s04_05_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/06 10:21:33 (UT+8) daisuke>
+# Time-stamp: <2024/03/11 21:30:11 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -21,7 +21,8 @@ import matplotlib.backends.backend_agg
 import matplotlib.figure
 
 # constructing a parser object
-parser = argparse.ArgumentParser (description='Plot of logarithmic scale')
+descr  = 'plotting an exponential function using linear scale'
+parser = argparse.ArgumentParser (description=descr)
 
 # adding arguments
 parser.add_argument ('-o', '--output', default='output.png', \
