606dc0ce96b3cd5d10ffe404e9fe8bd76d837265
Daisuke <mhdf23888@gmail.com>
2024-03-12 08:38:15 +0800

    updating s04/appy_s04_01_03.py

--- s04/appy_s04_01_03.py
+++ s04/appy_s04_01_03.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/06 10:19:55 (UT+8) daisuke>
+# Time-stamp: <2024/03/11 19:20:46 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -21,7 +21,7 @@ import matplotlib.backends.backend_agg
 import matplotlib.figure
 
 # constructing a parser object
-parser = argparse.ArgumentParser (description='A sample Matplotlib code')
+parser = argparse.ArgumentParser (description='plotting a circle')
 
 # adding arguments
 parser.add_argument ('-o', '--output', default='output.png', \
