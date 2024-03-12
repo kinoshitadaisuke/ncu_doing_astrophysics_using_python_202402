e5f79596dc27588123057746d8e282371588c82c
Daisuke <mhdf23888@gmail.com>
2024-03-12 08:38:33 +0800

    updating s04/appy_s04_04_00.py

--- s04/appy_s04_04_00.py
+++ s04/appy_s04_04_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/06 10:21:20 (UT+8) daisuke>
+# Time-stamp: <2024/03/11 21:21:11 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -21,7 +21,7 @@ import matplotlib.backends.backend_agg
 import matplotlib.figure
 
 # constructing a parser object
-parser = argparse.ArgumentParser (description='Data points with error bars')
+parser = argparse.ArgumentParser (description='attaching error bars')
 
 # adding arguments
 parser.add_argument ('-o', '--output', default='output.png', \
