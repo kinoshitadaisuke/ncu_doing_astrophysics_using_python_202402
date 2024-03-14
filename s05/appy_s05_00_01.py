ca4770dac88702651266650154c547135beead60
Daisuke <mhdf23888@gmail.com>
2024-03-14 09:25:25 +0800

    adding s05/appy_s05_00_01.py

--- s05/appy_s05_00_01.py
+++ s05/appy_s05_00_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/04 15:27:34 (CST) daisuke>
+# Time-stamp: <2024/03/13 14:55:47 (UT+8) daisuke>
 #
 
 # importing scipy module
