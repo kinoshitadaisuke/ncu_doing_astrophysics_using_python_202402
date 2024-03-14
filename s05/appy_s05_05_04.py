0de260a15c2cc03c5624f415cc1dd8cc0281c6e7
Daisuke <mhdf23888@gmail.com>
2024-03-14 09:37:31 +0800

    adding s05/appy_s05_05_04.py

--- s05/appy_s05_05_04.py
+++ s05/appy_s05_05_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/11 15:49:19 (CST) daisuke>
+# Time-stamp: <2024/03/13 14:59:02 (UT+8) daisuke>
 #
 
 # importing numpy module
