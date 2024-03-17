aaa1738377705968d1e704fe4af69ef33dd92bf1
Daisuke <mhdf23888@gmail.com>
2024-03-17 21:18:07 +0800

    updating s05/appy_s05_04_07.py

--- s05/appy_s05_04_07.py
+++ s05/appy_s05_04_07.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/13 14:58:38 (UT+8) daisuke>
+# Time-stamp: <2024/03/17 18:36:44 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -25,7 +25,7 @@ import matplotlib.backends.backend_agg
 import matplotlib.figure
 
 # constructing a parser object
-descr  = 'cubic interpolation'
+descr  = 'carrying out 4th order spline interpolation'
 parser = argparse.ArgumentParser (description=descr)
 
 # adding arguments
