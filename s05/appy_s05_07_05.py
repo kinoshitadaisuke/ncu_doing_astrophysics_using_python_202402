681ab513109a86f93e10d6ccc200ed56efdb4baa
Daisuke <mhdf23888@gmail.com>
2024-03-17 21:18:20 +0800

    updating s05/appy_s05_07_05.py

--- s05/appy_s05_07_05.py
+++ s05/appy_s05_07_05.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/13 15:00:13 (UT+8) daisuke>
+# Time-stamp: <2024/03/17 20:15:47 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -24,7 +24,7 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # constructing a parser object
-descr  = 'Finding minimum'
+descr  = 'Finding minimum of multiple variable function'
 parser = argparse.ArgumentParser (description=descr)
 
 # adding arguments
