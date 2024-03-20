d8bd12da53bd095f7decd21a5626d0a9b06778cc
Daisuke <mhdf23888@gmail.com>
2024-03-20 22:09:13 +0800

    updating s06/appy_s06_04_04.py

--- s06/appy_s06_04_04.py
+++ s06/appy_s06_04_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/20 07:27:27 (CST) daisuke>
+# Time-stamp: <2024/03/20 22:07:22 (UT+8) daisuke>
 #
 
 # importing sqlite module
