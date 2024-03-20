49e5b04663a95a714393ae64bf96b7cd94206235
Daisuke <mhdf23888@gmail.com>
2024-03-20 22:08:34 +0800

    updating s06/appy_s06_02_00.py

--- s06/appy_s06_02_00.py
+++ s06/appy_s06_02_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/19 14:08:40 (CST) daisuke>
+# Time-stamp: <2024/03/20 22:05:59 (UT+8) daisuke>
 #
 
 # importing urllib module
