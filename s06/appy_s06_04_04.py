b901e6745efe52baf7a589cd54f6fde6a144678b
Daisuke <mhdf23888@gmail.com>
2024-03-25 15:26:40 +0800

    updating s06/appy_s06_04_04.py

--- s06/appy_s06_04_04.py
+++ s06/appy_s06_04_04.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/20 22:07:22 (UT+8) daisuke>
+# Time-stamp: <2024/03/25 15:23:36 (UT+8) daisuke>
 #
 
 # importing sqlite module
@@ -32,6 +32,6 @@ with contextlib.closing (sqlite3.connect (file_db)) as conn:
     # printing results of query
     print (f'# name, a, e, i, node, peri, M, absmag')
     for result in results:
-        print (f'{result[0]:24s}  {result[1]:8.3f}  {result[2]:5.3f} ', \
+        print (f'{result[0]:24s}  {result[1]:8.2f}  {result[2]:5.3f} ', \
                f'{result[3]:6.2f} {result[4]:6.2f} {result[5]:6.2f} ', \
                f'{result[6]:6.2f}  {result[11]:7.2f}')
