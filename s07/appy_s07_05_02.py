9a3a6ae3f328811555ef8f6df5d7fbea40bf1a99
Daisuke <mhdf23888@gmail.com>
2024-03-28 09:00:56 +0800

    updating s07/appy_s07_05_02.py

--- s07/appy_s07_05_02.py
+++ s07/appy_s07_05_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/29 16:27:54 (CST) daisuke>
+# Time-stamp: <2024/03/28 08:56:49 (UT+8) daisuke>
 #
 
 # importing numpy module
