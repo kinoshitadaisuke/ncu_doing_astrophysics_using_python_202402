068f1d491b2afbb6e5d47e7adea7bbc15e242dc0
Daisuke <mhdf23888@gmail.com>
2024-04-02 11:50:15 +0800

    updating s07/appy_s07_03_02.py

--- s07/appy_s07_03_02.py
+++ s07/appy_s07_03_02.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/28 08:55:02 (UT+8) daisuke>
+# Time-stamp: <2024/04/02 10:41:38 (UT+8) daisuke>
 #
 
 # importing datetime module
@@ -11,7 +11,7 @@ import datetime
 import astropy.time
 
 # getting current date/time using datetime module
-now_datetime = datetime.datetime.utcnow ()
+now_datetime = datetime.datetime.now (tz=datetime.timezone.utc)
 
 # constructing Astropy's Time object from a string
 now = astropy.time.Time (now_datetime, scale='utc')
