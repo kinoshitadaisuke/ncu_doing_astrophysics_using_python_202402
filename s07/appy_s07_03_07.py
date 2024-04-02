55adce38c959593c95b8bcffbf29ba0c592fce06
Daisuke <mhdf23888@gmail.com>
2024-04-02 11:50:21 +0800

    updating s07/appy_s07_03_07.py

--- s07/appy_s07_03_07.py
+++ s07/appy_s07_03_07.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/28 08:55:25 (UT+8) daisuke>
+# Time-stamp: <2024/04/02 10:46:07 (UT+8) daisuke>
 #
 
 # importing astropy module
@@ -12,7 +12,7 @@ import astropy.units
 u_hr = astropy.units.hr
 
 # time t1
-t1 = astropy.time.Time ('2023-12-31 18:00:00', format='iso', scale='utc')
+t1 = astropy.time.Time ('2024-12-31 18:00:00', format='iso', scale='utc')
 
 # calculation of t1 + 12-hr
 t2 = t1 + 12.0 * u_hr
