32547f93d7f04e1a71ce81fc7aeb6c6739cfeb93
Daisuke <mhdf23888@gmail.com>
2024-04-02 11:50:02 +0800

    updating s07/appy_s07_00_05.py

--- s07/appy_s07_00_05.py
+++ s07/appy_s07_00_05.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/03/28 08:52:09 (UT+8) daisuke>
+# Time-stamp: <2024/04/01 21:20:24 (UT+8) daisuke>
 #
 
 # importing astropy module
@@ -24,6 +24,7 @@ print ()
 print (M_E)
 print ()
 
-# value of Jupiter mass in the unit of Solar mass
+# value of Jupiter mass in the unit of Solar mass and Earth mass
 print (f'1 M_J = {M_J}')
 print (f'      = {M_J / M_S:g} M_S')
+print (f'      = {M_J / M_E:g} M_E')
