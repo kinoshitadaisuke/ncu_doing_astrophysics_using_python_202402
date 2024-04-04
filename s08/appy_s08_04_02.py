d38ca953d2586a30e8a2988a41f9c69c629bd895
Daisuke <mhdf23888@gmail.com>
2024-04-04 20:28:59 +0800

    updating s08/appy_s08_04_02.py

--- s08/appy_s08_04_02.py
+++ s08/appy_s08_04_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/05 15:44:52 (CST) daisuke>
+# Time-stamp: <2024/04/04 20:26:21 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -15,10 +15,10 @@ import matplotlib.backends.backend_agg
 file_input = 'cmb_cobe.data'
 
 # output file name
-file_output = 'ai2023_s08_04_02.png'
+file_output = 'appy_s08_04_02.png'
 
 # resolution in DPI
-resolution_dpi = 225
+resolution_dpi = 150
 
 # numpy arrays for storing data
 data_freq_kayser = numpy.array ([])
