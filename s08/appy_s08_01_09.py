b78bc09ce31521eac0802d4bf91e4f1aade58c9b
Daisuke <mhdf23888@gmail.com>
2024-04-04 20:28:19 +0800

    updating s08/appy_s08_01_09.py

--- s08/appy_s08_01_09.py
+++ s08/appy_s08_01_09.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/02 12:31:19 (CST) daisuke>
+# Time-stamp: <2024/04/04 20:23:26 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -17,10 +17,10 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # output file name
-file_output = 'ai2023_s08_01_09.png'
+file_output = 'appy_s08_01_09.png'
 
 # resolution in DPI
-resolution_dpi = 225
+resolution_dpi = 150
 
 # units
 unit_K              = astropy.units.K
