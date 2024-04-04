ea05163ef501d6bdea57a8f913217421475d559d
Daisuke <mhdf23888@gmail.com>
2024-04-04 20:28:00 +0800

    updating s08/appy_s08_00_17.py

--- s08/appy_s08_00_17.py
+++ s08/appy_s08_00_17.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/10/31 20:28:22 (CST) daisuke>
+# Time-stamp: <2024/04/04 20:21:37 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -15,10 +15,10 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # output file name
-file_output = 'ai2023_s08_00_17.png'
+file_output = 'appy_s08_00_17.png'
 
 # resolution in DPI
-resolution_dpi = 225
+resolution_dpi = 150
 
 #
 # function to calculate blackbody curve
