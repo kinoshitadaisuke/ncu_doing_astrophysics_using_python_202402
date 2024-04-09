7a8382a11466af02d9a77e4aeb798d107fff38ad
Daisuke <mhdf23888@gmail.com>
2024-04-09 11:53:25 +0800

    updating s08/appy_s08_03_03.py on 2024-04-09T03:53:25.580524+00:00

--- s08/appy_s08_03_03.py
+++ s08/appy_s08_03_03.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/04/04 20:25:25 (UT+8) daisuke>
+# Time-stamp: <2024/04/09 11:18:50 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -134,8 +134,8 @@ canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
 ax     = fig.add_subplot (111)
 
 # labels
-ax.set_xlabel ('Wavelength [$\mu$m]')
-ax.set_ylabel ('Flux [Jy]')
+ax.set_xlabel (r'Wavelength [$\mu$m]')
+ax.set_ylabel (r'Flux [Jy]')
 
 # axes
 ax.set_xscale ('log')
