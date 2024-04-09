089892fed6772274c17687ceb0d9f4fbd82622df
Daisuke <mhdf23888@gmail.com>
2024-04-09 11:53:18 +0800

    updating s08/appy_s08_01_16.py on 2024-04-09T03:53:18.070125+00:00

--- s08/appy_s08_01_16.py
+++ s08/appy_s08_01_16.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/04/04 20:24:22 (UT+8) daisuke>
+# Time-stamp: <2024/04/09 11:08:09 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -84,7 +84,7 @@ c_v = c.value
 ax2 = ax.secondary_xaxis (location='top', \
                           functions=(lambda x: c_v/x * 10**6, \
                                      lambda x: c_v/x * 10**-6) )
-ax2.set_xlabel ('Frequency [Hz]')
+ax2.set_xlabel (r'Frequency [Hz]')
 
 # grid
 ax.grid ()
