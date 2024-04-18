e19855ad3b368ac45f9847d6d385b1ff0b71a516
Daisuke <mhdf23888@gmail.com>
2024-04-18 20:32:48 +0800

    updating s10/appy_s10_02_06.py on 2024-04-18T12:32:48.038952+00:00

--- s10/appy_s10_02_06.py
+++ s10/appy_s10_02_06.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/18 19:31:56 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/18 20:28:03 (UT+8) daisuke>
 #
 
 # importing json module
