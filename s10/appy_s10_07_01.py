a27ff58b75e8c8989ac8a5e33f47b6a2a1b93bcd
Daisuke <mhdf23888@gmail.com>
2024-04-18 20:33:51 +0800

    updating s10/appy_s10_07_01.py on 2024-04-18T12:33:50.718297+00:00

--- s10/appy_s10_07_01.py
+++ s10/appy_s10_07_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/19 19:57:45 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/18 20:31:07 (UT+8) daisuke>
 #
 
 # importing argparse module
