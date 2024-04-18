a15c52f493c3cfa0a47ab2884502a15e5a46ef61
Daisuke <mhdf23888@gmail.com>
2024-04-18 20:33:00 +0800

    updating s10/appy_s10_04_00.py on 2024-04-18T12:32:59.844074+00:00

--- s10/appy_s10_04_00.py
+++ s10/appy_s10_04_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/19 15:23:06 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/18 20:28:42 (UT+8) daisuke>
 #
 
 # importing urllib module
