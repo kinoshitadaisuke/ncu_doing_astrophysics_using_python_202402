b11b8b9619eb449d741ade6e28000f8beb08ac75
Daisuke <mhdf23888@gmail.com>
2024-04-18 20:33:02 +0800

    updating s10/appy_s10_04_01.py on 2024-04-18T12:33:01.995775+00:00

--- s10/appy_s10_04_01.py
+++ s10/appy_s10_04_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/19 15:49:16 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/18 20:28:46 (UT+8) daisuke>
 #
 
 # original CSV file
