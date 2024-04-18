7621655039a272e0b497567011cb4576f894ce7f
Daisuke <mhdf23888@gmail.com>
2024-04-18 20:32:22 +0800

    updating s10/appy_s10_01_01.py on 2024-04-18T12:32:22.206794+00:00

--- s10/appy_s10_01_01.py
+++ s10/appy_s10_01_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/18 16:29:29 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/18 20:26:49 (UT+8) daisuke>
 #
 
 # importing astropy module
