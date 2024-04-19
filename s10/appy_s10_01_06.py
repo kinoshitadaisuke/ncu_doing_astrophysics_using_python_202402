9076b9ccd434c4ad035be13d23deacdd6d2aeb5d
kinoshitadaisuke <mhdf23888@gmail.com>
2024-04-19 11:51:09 +0800

    updating s10/appy_s10_01_06.py on 2024-04-19T03:51:09.146380+00:00

--- s10/appy_s10_01_06.py
+++ s10/appy_s10_01_06.py
@@ -1,14 +1,14 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/04/18 20:27:11 (UT+8) daisuke>
+# Time-stamp: <2024/04/19 11:43:59 (UT+8) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
 # CSV file name
-file_csv = 'hyg/hyg/v3/hyg_v37.csv'
+file_csv = 'hyg/hyg/v3/hyg_v38.csv'
 
 # reading a CSV file and storing data in an astropy table
 table = astropy.io.ascii.read (file_csv, format='csv')
