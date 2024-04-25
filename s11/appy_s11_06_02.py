10a5dae7078066ed1fc84e42d8f91ac180a6277c
kinoshitadaisuke <mhdf23888@gmail.com>
2024-04-25 20:36:47 +0800

    updating s11/appy_s11_06_02.py on 2024-04-25T12:36:47.107311+00:00

--- s11/appy_s11_06_02.py
+++ s11/appy_s11_06_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/23 17:30:26 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/25 20:35:07 (UT+8) daisuke>
 #
 
 # importing argparse module
