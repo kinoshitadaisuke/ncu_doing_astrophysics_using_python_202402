24cbe2d5adaf54b71e8a7309b84cf2a79dc33b06
kinoshitadaisuke <mhdf23888@gmail.com>
2024-04-25 20:36:43 +0800

    updating s11/appy_s11_06_00.py on 2024-04-25T12:36:43.576877+00:00

--- s11/appy_s11_06_00.py
+++ s11/appy_s11_06_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/11/23 16:53:05 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/04/25 20:34:52 (UT+8) daisuke>
 #
 
 # importing urllib module
