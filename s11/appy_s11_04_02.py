1510cb9aa867b7659ac4df8027cf52abc7296c57
kinoshitadaisuke <mhdf23888@gmail.com>
2024-04-28 01:14:16 +0800

    updating s11/appy_s11_04_02.py on 2024-04-27T17:14:15.682165+00:00

---
+++ s11/appy_s11_04_02.py
@@ -0,0 +1,34 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/04/28 00:59:41 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing astropy module
+import astropy.io.votable
+
+# VOTable file name
+file_votable = "m3.vot.gz"
+
+# output file name
+file_output = 'm3.list'
+
+# reading VOTable
+data_vot = astropy.io.votable.parse_single_table (file_votable)
+
+# printing data type
+print (f'data type of "data_vot" = {type (data_vot)}')
+
+# converting VOTable data into Astropy table
+table = data_vot.to_table ()
+
+# printing data type
+print (f'data type of "table"    = {type (table)}')
+
+# printing column information of data read from VOTable file
+print (f'columns of Gaia data:')
+for column in table.columns:
+    print (f'  {column}')
