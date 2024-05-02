c9014ecc656478bbda3dad6e99ded3dd56a5f5d9
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-02 08:24:51 +0800

    updating s12/appy_s12_03_08.py on 2024-05-02T00:24:51.075526+00:00

--- s12/appy_s12_03_08.py
+++ s12/appy_s12_03_08.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/01 10:54:55 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/02 08:19:41 (UT+8) daisuke>
 #
 
 # importing numpy module
