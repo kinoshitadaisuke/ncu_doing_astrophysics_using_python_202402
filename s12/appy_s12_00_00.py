5da800420bb4efcbce40b3d2a0181cac11e29417
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-03 11:00:45 +0800

    updating s12/appy_s12_00_00.py on 2024-05-03T03:00:45.686078+00:00

--- s12/appy_s12_00_00.py
+++ s12/appy_s12_00_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/02 08:16:37 (UT+8) daisuke>
+# Time-stamp: <2024/05/03 10:39:44 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -10,6 +10,9 @@ import numpy
 # importing astropy module
 import astropy.time
 import astropy.units
+
+# output data file
+file_output = 'appy_s12_00_00.data'
 
 # constant
 pi = numpy.pi
@@ -36,12 +39,12 @@ delta = 2.0 * pi * 0.25
 mag_mean = 20.0
 
 # start of observation
-date_start = '2023-12-01T12:00:00'
+date_start = '2024-07-01T12:00:00'
 t_start    = astropy.time.Time (date_start, scale='utc', format='isot')
 mjd_start  = t_start.mjd
 
 # end of observation
-date_end = '2023-12-01T20:00:00'
+date_end = '2024-07-01T20:00:00'
 t_end    = astropy.time.Time (date_end, scale='utc', format='isot')
 mjd_end  = t_end.mjd
 
@@ -53,23 +56,6 @@ overhead = 10.0 * u_sec
 
 # interval between exposures (sec)
 interval = exptime + overhead
-
-# printing input parameters
-print (f"#")
-print (f"#")
-print (f"# Synthetic data for period search")
-print (f"#")
-print (f"#")
-print (f"# input parameters")
-print (f"#")
-print (f"#  start of obs. = {t_start} = MJD {t_start.mjd}")
-print (f"#  end of obs.   = {t_end} = MJD {t_end.mjd}")
-print (f"#  amplitude     = {A} mag")
-print (f"#  period        = {P}")
-print (f"#  delta         = {delta}")
-print (f"#  average mag.  = {mag_mean} mag")
-print (f"#  exposure time = {exptime}")
-print (f"#  overhead      = {overhead}")
 
 # function for sine curve
 def sine_curve (datetime, A, P, delta, mag_mean):
@@ -94,9 +80,33 @@ data_t = t_start + numpy.linspace (0.0, interval * (n_data - 1), n_data)
 # generating a numpy array for magnitude
 data_mag = sine_curve (data_t, A, P, delta, mag_mean)
 
-# printing generated synthetic data
-print (f"#")
-print (f"# date/time, MJD, magnitude")
-print (f"#")
-for i in range (len (data_t)):
-    print (f"{data_t[i]} {data_t[i].mjd:15.9f} {data_mag[i]:9.6f}")
+# opening file for writing
+with open (file_output, 'w') as fh:
+    # head of data file
+    header = f'#\n' \
+        + f'#\n' \
+        + f'# Synthetic data for period search\n' \
+        + f'#\n' \
+        + f'#\n' \
+        + f'#  input parameters\n' \
+        + f'#\n' \
+        + f'#   start of obs. = {t_start} = MJD {t_start.mjd}\n' \
+        + f'#   end of obs.   = {t_end} = MJD {t_end.mjd}\n' \
+        + f'#   amplitude     = {A} mag\n' \
+        + f'#   period        = {P}\n' \
+        + f'#   delta         = {delta}\n' \
+        + f'#   average mag.  = {mag_mean} mag\n' \
+        + f'#   exposure time = {exptime}\n' \
+        + f'#   overhead      = {overhead}\n' \
+        + f'#\n' \
+        + f'#  data format\n' \
+        + f'#\n' \
+        + f'#   date/time, MJD, magnitude\n' \
+        + f'#\n'
+    # writing input parameters to file
+    fh.write (header)
+
+    # writing generated synthetic data
+    for i in range (len (data_t)):
+        record = f'{data_t[i]} {data_t[i].mjd:15.9f} {data_mag[i]:9.6f}\n'
+        fh.write (record)
