15cd39a09c9966fe07951de3263fb3e3155db69a
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-03 12:04:52 +0800

    updating s12/appy_s12_02_06.py on 2024-05-03T04:04:51.784883+00:00

--- s12/appy_s12_02_06.py
+++ s12/appy_s12_02_06.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/02 08:18:34 (UT+8) daisuke>
+# Time-stamp: <2024/05/03 11:58:25 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -12,10 +12,10 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # input file name
-file_input = 'ai2023_s12_02_02.data'
+file_input = 'appy_s12_02_02.data'
 
 # output file name
-file_output = 'ai2023_s12_02_06.png'
+file_output = 'appy_s12_02_06.png'
 
 # empty numpy arrays for storing data
 data_per = numpy.array ([])
