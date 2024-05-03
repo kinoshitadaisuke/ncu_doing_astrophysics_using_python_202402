c42074dfbef94076f6450da5de3f9dcc50c370af
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-03 12:04:57 +0800

    updating s12/appy_s12_02_09.py on 2024-05-03T04:04:56.918439+00:00

--- s12/appy_s12_02_09.py
+++ s12/appy_s12_02_09.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/02 08:18:48 (UT+8) daisuke>
+# Time-stamp: <2024/05/03 12:02:24 (UT+8) daisuke>
 #
 
 # importing numpy module
@@ -15,7 +15,7 @@ import matplotlib.backends.backend_agg
 file_input = '201498.tb2.txt'
 
 # output file name
-file_output = 'ai2023_s12_02_09.png'
+file_output = 'appy_s12_02_09.png'
 
 # best fit period (day)
 p_best = 6.34378 / 24.0
