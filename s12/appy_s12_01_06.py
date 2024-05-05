e05356d97a3b88b8c324bba15021bfd5293c76b2
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-05 16:26:03 +0800

    updating s12/appy_s12_01_06.py on 2024-05-05T08:26:03.397063+00:00

--- s12/appy_s12_01_06.py
+++ s12/appy_s12_01_06.py
@@ -1,32 +1,36 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/03 11:31:20 (UT+8) daisuke>
+# Time-stamp: <2024/05/05 16:20:25 (UT+8) daisuke>
 #
 
 # importing numpy module
 import numpy
 
+# importing scipy module
+import scipy.optimize
+
 # importing matplotlib module
 import matplotlib.figure
 import matplotlib.backends.backend_agg
 
-# data file name
-file_input = 'appy_s12_01_00.data'
+# input data file
+file_input = 'appy_s12_01_02.data'
 
-# output file name
-file_output = 'appy_s12_01_06.png'
+# output figure file
+file_output = 'appy_s12_01_06.pdf'
 
-# best fit period (day)
-p_best = 7.5073 / 24.0
+# range of data for fitting
+x_min = 7.4
+x_max = 7.6
 
-# empty numpy arrays for storing data
-data_mjd   = numpy.array ([])
-data_phase = numpy.array ([])
-data_mag   = numpy.array ([])
-data_err   = numpy.array ([])
+# empty numpy array for storing data
+data_all_per = numpy.array ([])
+data_all_var = numpy.array ([])
+data_fit_per = numpy.array ([])
+data_fit_var = numpy.array ([])
 
-# opening file
+# opening file for reading
 with open (file_input, 'r') as fh:
     # reading file line-by-line
     for line in fh:
@@ -36,39 +40,85 @@ with open (file_input, 'r') as fh:
         # removing line feed at the end of line
         line = line.strip ()
         # splitting data
-        (datetime, mjd_str, mag_str, err_str) = line.split ()
+        (per_day_str, per_hr_str, per_min_str, var_str) = line.split ()
         # conversion from string into float
-        mjd = float (mjd_str)
-        mag = float (mag_str)
-        err = float (err_str)
-        phase = mjd / p_best - int (mjd / p_best)
+        per_hr = float (per_hr_str)
+        var = float (var_str)
         # appending the data at the end of numpy arrays
-        data_mjd   = numpy.append (data_mjd, mjd)
-        data_phase = numpy.append (data_phase, phase)
-        data_mag   = numpy.append (data_mag, mag)
-        data_err   = numpy.append (data_err, err)
+        data_all_per = numpy.append (data_all_per, per_hr)
+        data_all_var = numpy.append (data_all_var, var)
+        if ( (per_hr >= x_min) and (per_hr <= x_max) ):
+            data_fit_per = numpy.append (data_fit_per, per_hr)
+            data_fit_var = numpy.append (data_fit_var, var)
 
-# making objects "fig" and "ax"
+# initial values of coefficients of fitted function
+a = 1.0
+b = 1.0
+c = 1.0
+
+# function to be used for least-squares fitting
+def func (x, a, b, c):
+    y = a * (x - b)**2 + c
+    return y
+
+# least-squares fitting using scipy.optimize.curve_fit
+popt, pcov = scipy.optimize.curve_fit (func, data_fit_per, data_fit_var)
+
+# fitted coefficients
+print ("popt:")
+print (popt)
+
+# covariance matrix
+print ("pcov:")
+print (pcov)
+
+# fitted a and b
+a_fitted = popt[0]
+b_fitted = popt[1]
+c_fitted = popt[2]
+
+# degree of freedom
+dof = len (data_fit_per) - 3
+print (f"dof = {dof}")
+
+# residual
+residual = data_fit_var - func (data_fit_per, a_fitted, b_fitted, c_fitted)
+reduced_chi2 = (residual**2).sum () / dof
+print (f"reduced chi^2 = {reduced_chi2}")
+
+# errors of a and b
+a_err = numpy.sqrt (pcov[0][0])
+b_err = numpy.sqrt (pcov[1][1])
+c_err = numpy.sqrt (pcov[2][2])
+print (f"a = {a_fitted:8.5f} +/- {a_err:8.5f} ({a_err/a_fitted*100:6.2f} %)")
+print (f"b = {b_fitted:8.5f} +/- {b_err:8.5f} ({b_err/b_fitted*100:6.2f} %)")
+print (f"c = {c_fitted:8.5f} +/- {c_err:8.5f} ({c_err/c_fitted*100:6.2f} %)")
+
+# fitted line
+fitted_x = numpy.linspace (x_min, x_max, 10**3)
+fitted_y = func (fitted_x, a_fitted, b_fitted, c_fitted)
+
+# making fig and ax
 fig    = matplotlib.figure.Figure ()
 canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
 ax     = fig.add_subplot (111)
 
 # labels
-ax.set_xlabel ('Phase')
-ax.set_ylabel ('Apparent Magnitude [mag]')
+ax.set_xlabel ('Period [hr]')
+ax.set_ylabel ('Variance')
 
-# axes
-ax.invert_yaxis ()
+# range of plot
+ax.set_xlim (x_min - 0.1, x_max + 0.1)
+ax.set_ylim (0, func (x_max, a_fitted, b_fitted, c_fitted) * 1.5 )
 
-# plotting data
-ax.errorbar (data_phase, data_mag, yerr=data_err, \
-             linestyle='None', marker='o', markersize=5, color='blue', \
-             ecolor='black', capsize=5, \
-             label='folded lightcurve')
-ax.errorbar (data_phase + 1, data_mag, yerr=data_err, \
-             linestyle='None', marker='o', markersize=5, color='blue', \
-             ecolor='black', capsize=5)
+# plotting a figure
+ax.plot (data_all_per, data_all_var, \
+         linestyle='-', linewidth=5, color='blue', \
+         label='result of PDM analysis')
+ax.plot (fitted_x, fitted_y, \
+         linestyle=':', linewidth=3, color='red', \
+         label='fitted curve by curve_fit')
 ax.legend ()
 
-# saving the plot into a file
+# saving the figure to a file
 fig.savefig (file_output, dpi=150)
