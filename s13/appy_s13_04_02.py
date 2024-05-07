557dca71aa20feb568abf742b2fee0e2e71249fa
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-07 08:36:07 +0800

    updating s13/appy_s13_04_02.py on 2024-05-07T00:36:07.416392+00:00

--- s13/appy_s13_04_02.py
+++ s13/appy_s13_04_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/10 13:51:58 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/07 08:34:01 (UT+8) daisuke>
 #
 
 # importing argparse module
