54a0383253a5cf957f5caa872f6f9470bbf639fd
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-09 09:59:18 +0800

    updating s13/appy_s13_00_04.py on 2024-05-09T01:59:17.749685+00:00

--- s13/appy_s13_00_04.py
+++ s13/appy_s13_00_04.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/07 08:32:46 (UT+8) daisuke>
+# Time-stamp: <2024/05/09 09:20:37 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -13,214 +13,186 @@ import sys
 # importing pathlib module
 import pathlib
 
+# importing datetime module
+import datetime
+
 # importing numpy module
 import numpy
-import numpy.random
 
 # importing astropy module
-import astropy.io
+import astropy.io.fits
 import astropy.table
 
 # importing photutils module
 import photutils.datasets
 
 # constructing parser object
-descr  = 'generating a synthetic image with artificial stars and galaxies'
+descr  = 'generating a synthetic image of sky background + stars'
 parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-x', '--size-x', type=int, default=2048, \
-                     help='image size in X-axis (default: 2048)')
-parser.add_argument ('-y', '--size-y', type=int, default=2048, \
-                     help='image size in Y-axis (default: 2048)')
-parser.add_argument ('-n', '--nstars', type=int, default=100, \
-                     help='number of stars to generate (default: 100)')
-parser.add_argument ('-g', '--ngalaxies', type=int, default=10, \
-                     help='number of galaxies to generate (default: 10)')
-parser.add_argument ('-f', '--flux-min', type=float, default=1000.0, \
-                     help='minimum flux of stars (default: 1000)')
-parser.add_argument ('-p', '--fwhm-psf', type=float, default=3.5, \
-                     help='FWHM of PSF in pixel (default: 3.5)')
-parser.add_argument ('-d', '--fwhm-stddev', type=float, default=0.1, \
-                     help='stddev of FWHM distribution in pixel (default: 0.1)')
-parser.add_argument ('-q', '--fwhm-psf-gal', type=float, default=8.0, \
-                     help='FWHM of galaxy PSF in pixel (default: 8)')
-parser.add_argument ('-r', '--fwhm-psf-gal-stddev', type=float, default=4.0, \
-                     help='stddev of FWHM of galaxy PSF in pixel (default: 4)')
-parser.add_argument ('-s', '--sky', type=float, default=1000.0, \
-                     help='sky background level in ADU (default: 1000)')
-parser.add_argument ('-e', '--sky-stddev', type=float, default=30.0, \
-                     help='stddev of sky background in ADU (default: 30)')
-parser.add_argument ('-o', '--output-file', default='', \
+# adding command-line arguments
+parser.add_argument ('-b', '--background', type=float, default=1000.0, \
+                     help='background level (default: 1000)')
+parser.add_argument ('-s', '--sigma', type=float, default=10.0, \
+                     help='noise level (default: 10)')
+parser.add_argument ('-n', '--nstar', type=int, default=10, \
+                     help='number of stars to add (default: 10)')
+parser.add_argument ('-f', '--flux', type=float, default=10000.0, \
+                     help='maximum total flux of star (default: 10000)')
+parser.add_argument ('-p', '--psf', type=float, default=5.0, \
+                     help='FWHM of stellar radial profile (default: 5)')
+parser.add_argument ('-x', '--xsize', type=int, default=512, \
+                     help='image size in x-axis (default: 512)')
+parser.add_argument ('-y', '--ysize', type=int, default=512, \
+                     help='image size in y-axis (default: 512)')
+parser.add_argument ('-o', '--output', default='', \
                      help='output file name')
-parser.add_argument ('-l', '--log-file', default='', \
-                     help='log file name')
 
 # command-line argument analysis
 args = parser.parse_args ()
 
-# image size
-image_size_x = args.size_x
-image_size_y = args.size_y
-image_shape  = (image_size_x, image_size_y)
+# input parameters
+sky_background_level = args.background
+noise_level          = args.sigma
+nstar                = args.nstar
+flux_max             = args.flux
+psf_fwhm             = args.psf
+image_size_x         = args.xsize
+image_size_y         = args.ysize
+file_output          = args.output
 
-# number of stars and galaxies to generate
-nstars = args.nstars
-ngals  = args.ngalaxies
-
-# flux of faintest stars
-flux_min = args.flux_min
-
-# FWHM of PSF
-fwhm_x            = args.fwhm_psf
-fwhm_y            = args.fwhm_psf
-fwhm_stddev_x     = args.fwhm_stddev
-fwhm_stddev_y     = args.fwhm_stddev
-fwhm_gal_x        = args.fwhm_psf_gal
-fwhm_gal_y        = args.fwhm_psf_gal
-fwhm_gal_stddev_x = args.fwhm_psf_gal_stddev
-fwhm_gal_stddev_y = args.fwhm_psf_gal_stddev
-
-# sky background level and stddev
-sky_mean   = args.sky
-sky_stddev = args.sky_stddev
-
-# output file name and log file name
-file_output = args.output_file
-file_log    = args.log_file
-
-# making pathlib objects
+# making pathlib object
 path_output = pathlib.Path (file_output)
-path_log    = pathlib.Path (file_log)
 
-# check of output file name
+# checking output file name
+if (file_output == ''):
+    # printing message
+    print (f'ERROR:')
+    print (f'ERROR: You need to specify output file name.')
+    print (f'ERROR:')
+    # exit
+    sys.exit ()
+# output file must be a FITS file
 if not (path_output.suffix == '.fits'):
     # printing message
-    print ("ERROR: Output file must be a FITS file.")
+    print (f'ERROR:')
+    print (f'ERROR: Output file must be a FITS file.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
-
-# check of log file name
-if (file_log == ''):
-    # printing message
-    print ("ERROR: You need to specify log file name.")
-    # exit
-    sys.exit ()
-
-# existence check for output file
+# existence check of output file
 if (path_output.exists ()):
     # printing message
-    print ("ERROR: output file '%s' exists." % (file_output) )
+    print (f'ERROR:')
+    print (f'ERROR: Output file exists. Exiting...')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 
-# existence check for log file
-if (path_log.exists ()):
-    # printing message
-    print ("ERROR: log file '%s' exists." % (file_log) )
-    # exit
-    sys.exit ()
+# image size
+image_size = (image_size_x, image_size_y)
 
-# generating a new astropy table
-table_stars = astropy.table.Table ()
-table_gals  = astropy.table.Table ()
+# date/time
+now = datetime.datetime.now ().isoformat ()
 
-# random number generator
-rng = numpy.random.default_rng ()
+# command name
+command = sys.argv[0]
 
-# generating random numbers for stars
-position_x = rng.uniform (0, image_size_x, nstars)
-position_y = rng.uniform (0, image_size_y, nstars)
-theta_deg  = rng.uniform (0, 360, nstars)
-psf_x      = rng.normal (loc=fwhm_x, scale=fwhm_stddev_x, size=nstars)
-psf_y      = rng.normal (loc=fwhm_y, scale=fwhm_stddev_y, size=nstars)
-powerlaw   = rng.power (1.5, size=nstars)
-flux       = flux_min / powerlaw
+# printing message
+print (f'#')
+print (f'# input parameters')
+print (f'#')
+print (f'#  image size                = {image_size[0]} x {image_size[1]}')
+print (f'#  mean sky background level = {sky_background_level} ADU')
+print (f'#  noise level (stddev)      = {noise_level} ADU')
+print (f'#  number of stars to add    = {nstar}')
+print (f'#  FWHM of stellar PSF       = {psf_fwhm} pixel')
+print (f'#  max total flux of stars   = {flux_max} ADU')
+print (f'#')
 
-# generating random numbers for galaxies
-centre_gal_x   = rng.uniform (image_size_x * 0.3, image_size_x * 0.7)
-centre_gal_y   = rng.uniform (image_size_y * 0.3, image_size_y * 0.7)
-position_gal_x = rng.normal (loc=centre_gal_x, scale=300, size=ngals)
-position_gal_y = rng.normal (loc=centre_gal_y, scale=300, size=ngals)
-theta_gal_deg  = rng.uniform (0, 360, ngals)
-psf_gal_x      = rng.normal (loc=fwhm_gal_x, scale=fwhm_gal_stddev_x, \
-                             size=ngals)
-psf_gal_y      = rng.normal (loc=fwhm_gal_y, scale=fwhm_gal_stddev_y, \
-                             size=ngals)
-powerlaw_gal   = rng.power (2.0, size=ngals)
-flux_gal       = flux_min * 3 / powerlaw_gal
-
-# conversion from degree to radian
-theta_rad     = numpy.radians (theta_deg)
-theta_gal_rad = numpy.radians (theta_gal_deg)
-
-# adding data to the table of stars
-table_stars['amplitude'] = flux
-table_stars['x_mean']    = position_x
-table_stars['y_mean']    = position_y
-table_stars['x_stddev']  = psf_x
-table_stars['y_stddev']  = psf_y
-table_stars['theta']     = theta_rad
-
-# adding data to the table of galaxies
-table_gals['amplitude'] = flux_gal
-table_gals['x_mean']    = position_gal_x
-table_gals['y_mean']    = position_gal_y
-table_gals['x_stddev']  = psf_gal_x
-table_gals['y_stddev']  = psf_gal_y
-table_gals['theta']     = theta_gal_rad
-
-# writing positions of stars and galaxies to log file
-with open (file_log, 'w') as fh_log:
-    # information of stars
-    fh_log.write ("#\n")
-    fh_log.write ("# input parameters for producing synthetic image\n")
-    fh_log.write ("#\n")
-    fh_log.write ("#   image_size_x      = %d\n" % image_size_x)
-    fh_log.write ("#   image_size_y      = %d\n" % image_size_y)
-    fh_log.write ("#   nstars            = %d\n" % nstars)
-    fh_log.write ("#   ngals             = %d\n" % ngals)
-    fh_log.write ("#   flux_min          = %f\n" % flux_min)
-    fh_log.write ("#   fwhm_x            = %f\n" % fwhm_x)
-    fh_log.write ("#   fwhm_y            = %f\n" % fwhm_y)
-    fh_log.write ("#   fwhm_stddev_x     = %f\n" % fwhm_stddev_x)
-    fh_log.write ("#   fwhm_stddev_y     = %f\n" % fwhm_stddev_y)
-    fh_log.write ("#   fwhm_gal_x        = %f\n" % fwhm_gal_x)
-    fh_log.write ("#   fwhm_gal_y        = %f\n" % fwhm_gal_y)
-    fh_log.write ("#   fwhm_gal_stddev_x = %f\n" % fwhm_gal_stddev_x)
-    fh_log.write ("#   fwhm_gal_stddev_y = %f\n" % fwhm_gal_stddev_y)
-    fh_log.write ("#   sky_mean          = %f\n" % sky_mean)
-    fh_log.write ("#   sky_stddev        = %f\n" % sky_stddev)
-    fh_log.write ("#   file_output       = %s\n" % file_output)
-    fh_log.write ("#   file_log          = %s\n" % file_log)
-    fh_log.write ("#\n")
-    fh_log.write ("# information of stars\n")
-    fh_log.write ("#\n")
-    astropy.io.ascii.write (table_stars, fh_log, format='commented_header')
-    # information of galaxies
-    fh_log.write ("#\n")
-    fh_log.write ("# information of galaxies\n")
-    fh_log.write ("#\n")
-    astropy.io.ascii.write (table_gals, fh_log, format='commented_header')
-
-# generating stars
-image_stars = photutils.datasets.make_gaussian_sources_image (image_shape, \
-                                                              table_stars)
-
-# generating galaxies
-image_gals = photutils.datasets.make_gaussian_sources_image (image_shape, \
-                                                             table_gals)
+# printing status
+print (f'# now, generating background image...')
 
 # generating sky background
-image_sky = photutils.datasets.make_noise_image (image_shape, \
-                                                 distribution='gaussian', \
-                                                 mean=sky_mean, \
-                                                 stddev=sky_stddev)
-# generating synthetic image
-image = image_stars + image_gals + image_sky
+image_background \
+    = photutils.datasets.make_noise_image (image_size, \
+                                           distribution='gaussian', \
+                                           mean=sky_background_level, \
+                                           stddev=noise_level)
+
+# printing status
+print (f'# finished generating background image!')
+
+# printing status
+print (f'# now, generating source table...')
+
+# making a source table
+fwhm_sigma            = 2.0 * numpy.sqrt (2.0 * numpy.log (2.0) )
+source_table          = astropy.table.Table ()
+source_table['x_0']   = numpy.random.normal (image_size_x * 0.5, \
+                                             image_size_x * 0.1, nstar)
+source_table['y_0']   = numpy.random.normal (image_size_y * 0.5, \
+                                             image_size_y * 0.1, nstar)
+source_table['sigma'] = numpy.array ([psf_fwhm] * nstar) / fwhm_sigma
+source_table['flux']  = numpy.random.uniform (flux_max * 0.1, flux_max, nstar)
+
+# printing status
+print (f'# finished generating source table!')
+
+# printing source table
+print (f'# source_table:')
+for i in range (nstar):
+    print (f'#   a star of flux {source_table['flux'][i]:8.1f} ADU', \
+           f'at (x,y)=({source_table['x_0'][i]:7.1f},', \
+           f'{source_table['y_0'][i]:7.1f})')
+print (f'# total number of stars in source table = {len (source_table)}')
+
+# printing status
+print (f'# now, generating stars...')
+
+# generating stars
+image_star \
+    = photutils.datasets.make_gaussian_prf_sources_image (image_size, \
+                                                          source_table)
+
+# printing status
+print (f'# finished generating stars!')
+
+# printing status
+print (f'# now generating stars + background image...')
+
+# making synthetic image
+image = image_background + image_star
+
+# printing status
+print (f'# now, generating FITS header...')
 
 # preparing a FITS header
 header = astropy.io.fits.PrimaryHDU ().header
 
+# adding comments to the header
+header['history'] = f'FITS file created by the command "{command}"'
+header['history'] = f'Updated on {now}'
+header['comment'] = f'synthetic astronomical image simulating sky background'
+header['comment'] = f'Options given:'
+header['comment'] = f'  image size = {image_size_x} x {image_size_y}'
+header['comment'] = f'  mean sky background level = {sky_background_level} ADU'
+header['comment'] = f'  noise level  = {noise_level} ADU'
+header['comment'] = f'  number of stars  = {nstar}'
+header['comment'] = f'  FWHM of stellar PSF = {psf_fwhm} pix'
+header['comment'] = f'  max total flux of stars  = {flux_max} ADU'
+
+# printing status
+print (f'# finished generating FITS header!')
+
+# printing status
+print (f'# finished generating stars + background image!')
+
+# printing status
+print (f'# now, writing output FITS file...')
+
 # writing a FITS file
 astropy.io.fits.writeto (file_output, image, header=header)
+
+# printing status
+print (f'# finished writing output FITS file!')
