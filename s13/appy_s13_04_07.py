7483176faa6c15acd361fb53a4f4e265d276921d
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-11 20:26:56 +0800

    updating s13/appy_s13_04_07.py on 2024-05-11T12:26:55.897876+00:00

--- s13/appy_s13_04_07.py
+++ s13/appy_s13_04_07.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/07 08:34:22 (UT+8) daisuke>
+# Time-stamp: <2024/05/11 20:23:22 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -61,55 +61,73 @@ path_fits1  = pathlib.Path (file_fits1)
 path_fits2  = pathlib.Path (file_fits2)
 path_output = pathlib.Path (file_output)
 
+# date/time
+now = datetime.datetime.now ().isoformat ()
+
+# command name
+command = sys.argv[0]
+
 # check of output file name
 if not (path_output.suffix == '.fits'):
     # printing message
-    print ("Output file name must be a FITS file.")
+    print (f'ERROR:')
+    print (f'ERROR: Output file name must be a FITS file.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 
 # check of catalogue file name
 if not ( (path_cat1.suffix == '.cat') and (path_cat2.suffix == '.cat') ):
     # printing message
-    print ("Input file must be a catalogue file (*.cat).")
-    print ("catalogue file 1 = %s" % file_cat1)
-    print ("catalogue file 2 = %s" % file_cat2)
+    print (f'ERROR: Input file must be a catalogue file (*.cat).')
+    print (f'ERROR: catalogue file 1 = "{file_cat1}"')
+    print (f'ERROR: catalogue file 2 = "{file_cat2}"')
     # exit
     sys.exit ()
 
 # check of FITS file name
 if not ( (path_fits1.suffix == '.fits') and (path_fits2.suffix == '.fits') ):
     # printing message
-    print ("Input file must be a FITS file (*.fits).")
-    print ("FITS file 1 = %s" % file_fits1)
-    print ("FITS file 2 = %s" % file_fits2)
+    print (f'ERROR: Input file must be a FITS file (*.fits).')
+    print (f'ERROR: FITS file 1 = "{file_fits1}"')
+    print (f'ERROR: FITS file 2 = "{file_fits2}"')
     # exit
     sys.exit ()
 
 # existence checks
 if not (path_cat1.exists ()):
     # printing message
-    print ("ERROR: file '%s' does not exist." % file_cat1)
+    print (f'ERROR:')
+    print (f'ERROR: file "{file_cat1}" does not exist.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 if not (path_cat2.exists ()):
     # printing message
-    print ("ERROR: file '%s' does not exist." % file_cat2)
+    print (f'ERROR:')
+    print (f'ERROR: file "{file_cat2}" does not exist.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 if not (path_fits1.exists ()):
     # printing message
-    print ("ERROR: file '%s' does not exist." % file_fits1)
+    print (f'ERROR:')
+    print (f'ERROR: file "{file_fits1}" does not exist.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 if not (path_fits2.exists ()):
     # printing message
-    print ("ERROR: file '%s' does not exist." % file_fits2)
+    print (f'ERROR:')
+    print (f'ERROR: file "{file_fits2}" does not exist.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 if (path_output.exists ()):
     # printing message
-    print ("ERROR: file '%s' exists." % file_output)
+    print (f'ERROR:')
+    print (f'ERROR: file "{file_output}" exists.')
+    print (f'ERROR:')
     # exit
     sys.exit ()
 
@@ -138,8 +156,8 @@ list_source1_x = list (table_source1['xcentroid'])
 list_source1_y = list (table_source1['ycentroid'])
 list_source2_x = list (table_source2['xcentroid'])
 list_source2_y = list (table_source2['ycentroid'])
-position_1 = numpy.transpose ( (list_source1_x, list_source1_y) )
-position_2 = numpy.transpose ( (list_source2_x, list_source2_y) )
+position_1     = numpy.transpose ( (list_source1_x, list_source1_y) )
+position_2     = numpy.transpose ( (list_source2_x, list_source2_y) )
 
 # finding star-to-star matching
 transf, (list_matched_2, list_matched_1) \
@@ -150,34 +168,32 @@ list_matched_2_aligned \
     = astroalign.matrix_transform (list_matched_2, transf.params)
 
 # printing results
-print ("#")
-print ("# result of image alignment")
-print ("#")
-print ("#   date/time = %s" % now)
-print ("#")
-print ("# input files")
-print ("#")
-print ("#   catalogue file 1 = %s" % file_cat1)
-print ("#   catalogue file 2 = %s" % file_cat2)
-print ("#")
-print ("# transformation matrix")
-print ("#")
-print ("# [")
-print ("#   [%f, %f, %f]," \
-       % (transf.params[0][0], transf.params[0][1], transf.params[0][2]) )
-print ("#   [%f, %f, %f]," \
-       % (transf.params[1][0], transf.params[1][1], transf.params[1][2]) )
-print ("#   [%f, %f, %f]" \
-       % (transf.params[2][0], transf.params[2][1], transf.params[2][2]) )
-print ("# ]")
-print ("#")
-print ("#")
-print ("# list of matched stars")
-print ("#")
+print (f'#')
+print (f'# result of image alignment')
+print (f'#')
+print (f'#   date/time = {now}')
+print (f'#')
+print (f'# input files')
+print (f'#')
+print (f'#   catalogue file 1 = {file_cat1}')
+print (f'#   catalogue file 2 = {file_cat2}')
+print (f'#')
+print (f'# transformation matrix')
+print (f'#')
+print (f'# [')
+print (f'#  [{transf.params[0][0]:11.6f}, {transf.params[0][1]:11.6f}, {transf.params[0][2]:11.6f}],')
+print (f'#  [{transf.params[1][0]:11.6f}, {transf.params[1][1]:11.6f}, {transf.params[1][2]:11.6f}],')
+print (f'#  [{transf.params[2][0]:11.6f}, {transf.params[2][1]:11.6f}, {transf.params[2][2]:11.6f}]')
+print (f'# ]')
+print (f'#')
+print (f'#')
+print (f'# list of matched stars')
+print (f'#')
 for i in range ( len (list_matched_1) ):
-    print ("(%10.4f, %10.4f) on 1st image ==> (%10.4f, %10.4f) on 2nd image" \
-           % (list_matched_1[i][0], list_matched_1[i][1], \
-              list_matched_2[i][0], list_matched_2[i][1]) )
+    print (f'({list_matched_1[i][0]:8.3f}, {list_matched_1[i][1]:8.3f})', \
+           f'on 1st image', \
+           f'==> ({list_matched_2[i][0]:8.3f}, {list_matched_2[i][1]:8.3f})', \
+           f'on 2nd image')
 
 # reading FITS files
 (header1, image1) = read_fits (file_fits1)
@@ -194,6 +210,23 @@ st = skimage.transform.SimilarityTransform (scale=transf.scale, \
                                             rotation=transf.rotation, \
                                             translation=transf.translation)
 image2_aligned = skimage.transform.warp (image2, st.inverse)
+
+# adding some keywords to header
+header2['history'] = f'FITS file created by the command "{command}"'
+header2['history'] = f'Updated on {now}'
+header2['comment'] = f'Alignment of two astronomical images'
+header2['comment'] = f'Options given:'
+header2['comment'] = f' file_cat1   = "{file_cat1}"'
+header2['comment'] = f' file_cat2   = "{file_cat2}"'
+header2['comment'] = f' file_fits1  = "{file_fits1}"'
+header2['comment'] = f' file_fits2  = "{file_fits2}"'
+header2['comment'] = f' file_output = "{file_output}"'
+header2['comment'] = f'Transformation matrix:'
+header2['comment'] = f' ['
+header2['comment'] = f'  [{transf.params[0][0]:11.6f}, {transf.params[0][1]:11.6f}, {transf.params[0][2]:11.6f}]'
+header2['comment'] = f'  [{transf.params[1][0]:11.6f}, {transf.params[1][1]:11.6f}, {transf.params[1][2]:11.6f}]'
+header2['comment'] = f'  [{transf.params[2][0]:11.6f}, {transf.params[2][1]:11.6f}, {transf.params[2][2]:11.6f}]'
+header2['comment'] = f' ]'
 
 # writing a new FITS file
 astropy.io.fits.writeto (file_output, image2_aligned, header=header2)
