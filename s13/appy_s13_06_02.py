719dc47165c76dcc8b747775e5a1cc90ba3af537
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-12 22:07:55 +0800

    updating s13/appy_s13_06_02.py on 2024-05-12T14:07:52.398435+00:00

---
+++ s13/appy_s13_06_02.py
@@ -0,0 +1,52 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/05/12 17:49:21 (UT+8) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+# importing sys module
+import sys
+
+# importing pathlib module
+import pathlib
+
+# importing datetime module
+import datetime
+
+# importing astropy module
+import astropy.units
+import astropy.coordinates
+
+# importing astroquery module
+import astroquery.esa.jwst
+
+# date/time
+now = datetime.datetime.now ().isoformat ()
+
+# command name
+command = sys.argv[0]
+
+# constructing parser object
+descr  = "getting data product list of JWST data"
+parser = argparse.ArgumentParser (description=descr)
+
+# adding arguments
+parser.add_argument ('-i', '--obsid', default='', \
+                     help='observation ID of JWST data')
+
+# command-line argument analysis
+args = parser.parse_args ()
+
+# input parameters
+obsid = args.obsid
+
+# getting product list
+product_list = astroquery.esa.jwst.Jwst.get_product_list (observation_id=obsid)
+
+# printing product list
+print (f'# file name, product type, calib. level, availability')
+for i in range (len (product_list)):
+    print (f'{product_list["filename"][i]:56s} {product_list["producttype"][i]:10s} {product_list["calibrationlevel"][i]:2d} {product_list["public"][i]}')
