b0ebb90e882740bcde35a273801f7497beccad51
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-13 20:44:45 +0800

    updating s13/appy_s13_07_01.py on 2024-05-13T12:44:45.361160+00:00

--- s13/appy_s13_07_01.py
+++ s13/appy_s13_07_01.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/12 21:50:36 (UT+8) daisuke>
+# Time-stamp: <2024/05/13 20:21:32 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -9,6 +9,8 @@ import argparse
 
 # importing astropy module
 import astropy.io.votable
+import astropy.coordinates
+import astropy.units
 
 # construction of parser object for argparse
 descr  = 'reading VOTable file'
@@ -25,6 +27,9 @@ args = parser.parse_args ()
 file_votable = args.input
 file_output  = args.output
 
+# units
+u_deg = astropy.units.deg
+
 # reading VOTable
 table = astropy.io.votable.parse_single_table (file_votable).to_table ()
 
@@ -35,11 +40,16 @@ with open (file_output, 'w') as fh:
     fh.write (header)
     # writing data
     for i in range (len (table)):
-        record = f'{table["observation_id"][i]}' \
+        # coordinate
+        coord = astropy.coordinates.SkyCoord (table["ra"][i], table["dec"][i], \
+                                              unit=(u_deg, u_deg), frame='icrs')
+        # writing data to file
+        record = f'{table["observation_id"][i]:36s}' \
             + f' {table["exposure_duration"][i]:6.1f}' \
-            + f' {table["instrument_name"][i]}' \
-            + f' {table["obs_type"][i]}' \
-            + f' {table["data_product_type"][i]}' \
-            + f' {table["filter"][i]}' \
-            + f' {table["start_time"][i]}\n'
+            + f' {table["instrument_name"][i]:8s}' \
+            + f' {table["obs_type"][i]:28s}' \
+            + f' {table["data_product_type"][i]:8s}' \
+            + f' {table["filter"][i]:12s}' \
+            + f' {coord.ra.deg:8.3f}' \
+            + f' {coord.dec.deg:8.3f}\n'
         fh.write (record)
