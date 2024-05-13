991d6b690f712ee55d979fb4088729a100b882a3
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-13 20:44:41 +0800

    updating s13/appy_s13_07_00.py on 2024-05-13T12:44:41.357337+00:00

--- s13/appy_s13_07_00.py
+++ s13/appy_s13_07_00.py
@@ -1,8 +1,11 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/12 21:34:06 (UT+8) daisuke>
+# Time-stamp: <2024/05/13 19:30:25 (UT+8) daisuke>
 #
+
+# importing argparse module
+import argparse
 
 # importing astropy module
 import astropy.coordinates
@@ -10,8 +13,26 @@ import astropy.coordinates
 # importing astroquery module
 import astroquery.esa.hubble
 
-# output file name
-file_output = 'hubble_search_result.vot.gz'
+# construction of parser object for argparse
+descr  = 'searching for HST data'
+parser = argparse.ArgumentParser (description=descr)
+
+# adding arguments
+parser.add_argument ('-r', '--radius', type=float, default=1.0, \
+                     help='search radius in arcmin (default: 1)')
+parser.add_argument ('-o', '--output', default='', \
+                     help='output file name')
+parser.add_argument ('ra', nargs=1, help='RA of centre of search field')
+parser.add_argument ('dec', nargs=1, help='Dec of centre of search field')
+
+# command-line argument analysis
+args = parser.parse_args ()
+
+# input parameters
+radius_arcmin = args.radius
+file_output   = args.output
+ra            = args.ra[0]
+dec           = args.dec[0]
 
 # units
 u_ha     = astropy.units.hourangle
@@ -19,13 +40,13 @@ u_deg    = astropy.units.deg
 u_arcmin = astropy.units.arcmin
 
 # radius of search
-radius = 0.5 * u_arcmin
+radius = radius_arcmin * u_arcmin
 
 # making esahubble object
 esahubble = astroquery.esa.hubble.ESAHubble ()
 
 # coordinate
-coord = astropy.coordinates.SkyCoord ("04:16:34.49", "-24:04:58.7", \
+coord = astropy.coordinates.SkyCoord (ra, dec, \
                                       unit=(u_ha, u_deg), frame='icrs')
 
 # search
