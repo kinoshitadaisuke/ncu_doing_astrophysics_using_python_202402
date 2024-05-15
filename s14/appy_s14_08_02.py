23c24f0e018214445904fce47fc3483645d3299d
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-15 08:38:34 +0800

    updating s14/appy_s14_08_02.py on 2024-05-15T00:38:34.095019+00:00

--- s14/appy_s14_08_02.py
+++ s14/appy_s14_08_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/17 12:10:32 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/15 08:36:07 (UT+8) daisuke>
 #
 
 # importing pathlib module
