4125b27e77c877794c83df9e24244ca0c392963c
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-15 08:38:25 +0800

    updating s14/appy_s14_07_03.py on 2024-05-15T00:38:25.362205+00:00

--- s14/appy_s14_07_03.py
+++ s14/appy_s14_07_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/16 15:24:13 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/15 08:35:48 (UT+8) daisuke>
 #
 
 # importing datetime module
