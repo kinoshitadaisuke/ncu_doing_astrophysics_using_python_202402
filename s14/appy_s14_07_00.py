70ee1a5b9582c54a9aa251a33cb2bb6bf2d5bfae
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-15 08:38:19 +0800

    updating s14/appy_s14_07_00.py on 2024-05-15T00:38:18.714921+00:00

--- s14/appy_s14_07_00.py
+++ s14/appy_s14_07_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/15 09:30:24 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/15 08:35:37 (UT+8) daisuke>
 #
 
 # importing gzip module
