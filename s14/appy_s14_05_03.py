6a0a46a8d54f1d1272cb1fe062691818fa2a9ee0
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-21 08:55:30 +0800

    updating s14/appy_s14_05_03.py on 2024-05-21T00:55:29.715810+00:00

--- s14/appy_s14_05_03.py
+++ s14/appy_s14_05_03.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/15 08:34:56 (UT+8) daisuke>
+# Time-stamp: <2024/05/20 21:58:11 (UT+8) daisuke>
 #
 
 # importing subprocess module
@@ -12,7 +12,7 @@ ffmpeg = 'ffmpeg'
 
 # ffmpeg options
 options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
-    + f' -i comets/%04d.png' \
+    + f' -i comets/comets_%04d.png' \
     + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
 
 # output file name
