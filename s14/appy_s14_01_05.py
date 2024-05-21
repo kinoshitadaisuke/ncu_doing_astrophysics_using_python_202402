27ea62667397e5304c1c92d6e669ef5a671975ee
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-21 08:54:44 +0800

    updating s14/appy_s14_01_05.py on 2024-05-21T00:54:43.812296+00:00

--- s14/appy_s14_01_05.py
+++ s14/appy_s14_01_05.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/15 08:32:35 (UT+8) daisuke>
+# Time-stamp: <2024/05/20 18:15:52 (UT+8) daisuke>
 #
 
 # importing argparse module
@@ -23,7 +23,7 @@ list_method = [None, 'smallbody', 'designation', \
                'name', 'asteroid_name', 'comet_name']
 
 # adding arguments
-parser.add_argument ('-t', '--datetime', default='2024-01-01T12:00:00', \
+parser.add_argument ('-t', '--datetime', default='2000-01-01T12:00:00', \
                      help='date/time in UTC as YYYY-MM-DDThh:mm:ss format')
 parser.add_argument ('-l', '--length', type=float, default=10.0, \
                      help='length of ephemeris retrieval in day (default: 10)')
@@ -86,4 +86,4 @@ for data in ephemerides:
     print (f'  Equatorial:  (RA,Dec)=({data["RA"]:10.6f}, {data["DEC"]:+10.6f}) [deg]')
     print (f'  Ecliptic:   (Lon,Lat)=({data["ObsEclLon"]:10.6f}, {data["ObsEclLat"]:+10.6f}) [deg]')
     print (f'  Galactic:   (Lon,Lat)=({data["GlxLon"]:10.6f}, {data["GlxLat"]:+10.6f}) [deg]')
-    print (f'  Horizontal: (Lon,Lat)=({data["AZ"]:10.6f}, {data["EL"]:+10.6f}) [deg]')
+    print (f'  Horizontal:   (Az,El)=({data["AZ"]:10.6f}, {data["EL"]:+10.6f}) [deg]')
