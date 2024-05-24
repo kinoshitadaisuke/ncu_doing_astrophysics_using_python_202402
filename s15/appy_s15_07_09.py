a7108bbda99eb4672050adbf6ec55ceb16d79000
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-24 08:59:03 +0800

    updating s15/appy_s15_07_09.py on 2024-05-24T00:59:02.783927+00:00

--- s15/appy_s15_07_09.py
+++ s15/appy_s15_07_09.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/24 15:56:48 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/24 08:56:20 (UT+8) daisuke>
 #
 
 # importing scikit-learn module
