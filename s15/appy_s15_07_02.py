48e87b9b436ec103551061dab88f829934262ed6
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-24 08:58:47 +0800

    updating s15/appy_s15_07_02.py on 2024-05-24T00:58:46.776815+00:00

--- s15/appy_s15_07_02.py
+++ s15/appy_s15_07_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.10
+#!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2023/12/24 00:29:09 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2024/05/24 08:55:37 (UT+8) daisuke>
 #
 
 # importing numpy module
