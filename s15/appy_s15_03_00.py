fce5fc54b28f0283f7908f2ffe181d9de4304dff
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-26 19:11:32 +0800

    updating s15/appy_s15_03_00.py on 2024-05-26T11:11:32.157990+00:00

--- s15/appy_s15_03_00.py
+++ s15/appy_s15_03_00.py
@@ -1,11 +1,14 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/24 08:46:20 (UT+8) daisuke>
+# Time-stamp: <2024/05/26 19:05:11 (UT+8) daisuke>
 #
 
 # importing numpy module
 import numpy
+
+# output file name
+file_output = 'appy_s15_03_00.data'
 
 # making a random number generator
 rng = numpy.random.default_rng ()
@@ -28,9 +31,16 @@ data_0y = A_0 * numpy.sin (k_0 * data_0x + delta_0) + C_0 + noise0
 data_1x = rng.uniform (low=1.0, high=15.0, size=n_1)
 data_1y = A_1 * numpy.sin (k_1 * data_1x + delta_1) + C_1 + noise1
 
-# printing data
-print (f'# feature X, feature Y, classification')
-for i in range (data_0x.size):
-    print (f'{data_0x[i]:8.4f} {data_0y[i]:8.4f} A')
-for i in range (data_1x.size):
-    print (f'{data_1x[i]:8.4f} {data_1y[i]:8.4f} B')
+# opening file for writing
+with open (file_output, 'w') as fh:
+    # header of output file
+    header = f'# feature X, feature Y, classification\n'
+    # writing header to output file
+    fh.write (header)
+    # writing data to output file
+    for i in range (data_0x.size):
+        record = f'{data_0x[i]:8.4f} {data_0y[i]:8.4f} A\n'
+        fh.write (record)
+    for i in range (data_1x.size):
+        record = f'{data_1x[i]:8.4f} {data_1y[i]:8.4f} B\n'
+        fh.write (record)
