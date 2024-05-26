fb684a69a0b920914ffb1460d791ac4fa508fad6
kinoshitadaisuke <mhdf23888@gmail.com>
2024-05-26 19:11:14 +0800

    updating s15/appy_s15_01_00.py on 2024-05-26T11:11:14.218347+00:00

--- s15/appy_s15_01_00.py
+++ s15/appy_s15_01_00.py
@@ -1,11 +1,14 @@
 #!/usr/pkg/bin/python3.12
 
 #
-# Time-stamp: <2024/05/24 08:45:23 (UT+8) daisuke>
+# Time-stamp: <2024/05/26 18:36:19 (UT+8) daisuke>
 #
 
 # importing numpy module
 import numpy
+
+# output file name
+file_output = 'appy_s15_01_00.data'
 
 # making a random number generator
 rng = numpy.random.default_rng ()
@@ -19,9 +22,16 @@ n_1    = 200
 data_0 = rng.multivariate_normal (mean=mean_0, cov=covar, size=n_0)
 data_1 = rng.multivariate_normal (mean=mean_1, cov=covar, size=n_1)
 
-# printing data
-print (f'# value of feature X, value of feature Y, classification')
-for i in range (data_0.shape[0]):
-    print (f'{data_0[i,0]:8.4f} {data_0[i,1]:8.4f} A')
-for i in range (data_1.shape[0]):
-    print (f'{data_1[i,0]:8.4f} {data_1[i,1]:8.4f} B')
+# opening file for writing
+with open (file_output, 'w') as fh:
+    # header of output file
+    header = f'# value of feature X, value of feature Y, classification\n'
+    # writing header to output file
+    fh.write (header)
+    # writing data to output file
+    for i in range (data_0.shape[0]):
+        record = f'{data_0[i,0]:8.4f} {data_0[i,1]:8.4f} A\n'
+        fh.write (record)
+    for i in range (data_1.shape[0]):
+        record = f'{data_1[i,0]:8.4f} {data_1[i,1]:8.4f} B\n'
+        fh.write (record)
